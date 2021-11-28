2bda9a2a4bcdd7a5b205b1f65e5aca40b9e542f8
SEKUN <sekun@hey.com>
2021-11-28 13:17:30 +0800

    Move film ID, and homeworld ID to a module

---
+++ src/SwapiClient/Id.hs
@@ -0,0 +1,43 @@
+module SwapiClient.Id
+  ( FilmId (FilmId)
+  ) where
+
+import Data.Aeson (FromJSON (parseJSON), ToJSON (toJSON), Value (Number))
+import qualified Data.Aeson as Aeson (withScientific)
+import Data.Kind (Type)
+import qualified Data.Scientific as Scientific (toBoundedInteger)
+
+--------------------------------------------------------------------------------
+-- DATA TYPES
+
+newtype FilmId = FilmId Int
+  deriving Show
+
+newtype HomeworldId = HomeworldId Int
+  deriving Show
+
+--------------------------------------------------------------------------------
+-- INSTANCES
+-- TODO(sekun): Parse URL rather than assume it's a number (because it isn't).
+
+instance FromJSON (FilmId :: Type) where
+  parseJSON =
+    Aeson.withScientific "FilmId" $
+      \filmId ->
+        case Scientific.toBoundedInteger filmId of
+         Just intId -> pure $ FilmId intId
+         Nothing -> fail "ERROR: Film id must be a valid integer."
+
+instance ToJSON (FilmId :: Type) where
+  toJSON (FilmId filmId) = Number . fromIntegral $ filmId
+
+instance FromJSON (HomeworldId :: Type) where
+  parseJSON =
+    Aeson.withScientific "HomeworldId" $
+      \homeworldId ->
+        case Scientific.toBoundedInteger homeworldId of
+          Just intId -> pure $ HomeworldId intId
+          Nothing -> fail "ERROR: Homeworld id must be a valid integer"
+
+instance ToJSON (HomeworldId :: Type) where
+  toJSON (HomeworldId homeworldId) = Number . fromIntegral $ homeworldId
