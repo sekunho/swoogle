8040863201f224ea8882515db5be757285fe6c69
SEKUN <sekun@hey.com>
2021-11-29 18:15:18 +0800

    Refactor `HairColor`

--- src/SwapiClient.hs
+++ src/SwapiClient.hs
@@ -3,9 +3,50 @@ module SwapiClient
   , PersonName (PersonName)
   , Height (Height, UnknownHeight)
   , Mass (Mass, UnknownMass)
-  , HairColor (HairColor)
-  , SkinColor (SkinColor)
-  , EyeColor (EyeColor)
+  , HairColors (HairColors)
+  , HairColor
+    ( AuburnHair
+    , BlackHair
+    , BlondHair
+    , BrownHair
+    , GreyHair
+    , NoHairColor
+    , WhiteHair
+    )
+  , SkinColor
+    ( BlueSkin
+    , BrownSkin
+    , BrownMottleSkin
+    , DarkSkin
+    , FairSkin
+    , GoldSkin
+    , GreenSkin
+    , GreenTanSkin
+    , GreySkin
+    , LightSkin
+    , MetalSkin
+    , MottledGreenSkin
+    , OrangeSkin
+    , PaleSkin
+    , RedSkin
+    , SilverSkin
+    , TanSkin
+    , UnknownSkinColor
+    , WhiteSkin
+    , YellowSkin
+    )
+  , EyeColor
+    ( BlackEye
+    , BlueEye
+    , BlueGreyEye
+    , GoldEye
+    , HazelEye
+    , OrangeEye
+    , PinkEye
+    , RedEye
+    , UnknownEyeColor
+    , YellowEye
+    )
   , BirthYear (ABY, BBY, UnknownBirthYear)
   , HomeworldId
   , FilmId
--- src/SwapiClient/Person.hs
+++ src/SwapiClient/Person.hs
@@ -1,9 +1,49 @@
 module SwapiClient.Person
   ( BirthYear (BBY, ABY, UnknownBirthYear)
-  , Color (Blue, Yellow, Red, Hazel, NoColor)
-  , HairColor (HairColor)
-  , SkinColor (SkinColor)
-  , EyeColor (EyeColor)
+  , HairColors (HairColors)
+  , HairColor
+    ( AuburnHair
+    , BlackHair
+    , BlondHair
+    , BrownHair
+    , GreyHair
+    , NoHairColor
+    , WhiteHair
+    )
+  , SkinColor
+    ( BlueSkin
+    , BrownSkin
+    , BrownMottleSkin
+    , DarkSkin
+    , FairSkin
+    , GoldSkin
+    , GreenSkin
+    , GreenTanSkin
+    , GreySkin
+    , LightSkin
+    , MetalSkin
+    , MottledGreenSkin
+    , OrangeSkin
+    , PaleSkin
+    , RedSkin
+    , SilverSkin
+    , TanSkin
+    , UnknownSkinColor
+    , WhiteSkin
+    , YellowSkin
+    )
+  , EyeColor
+    ( BlackEye
+    , BlueEye
+    , BlueGreyEye
+    , GoldEye
+    , HazelEye
+    , OrangeEye
+    , PinkEye
+    , RedEye
+    , UnknownEyeColor
+    , YellowEye
+    )
   , Height (Height, UnknownHeight)
   , Mass (Mass, UnknownMass)
   , Gender (Male, Female)
@@ -13,8 +53,8 @@ module SwapiClient.Person
       , pHeight
       , pMass
       , pHairColor
-      , pSkinColor
-      , pEyeColor
+    --  , pSkinColor
+    --  , pEyeColor
       , pBirthYear
       , pGender
       , pHomeworldId
@@ -23,8 +63,17 @@ module SwapiClient.Person
   ) where
 
 import Data.Aeson
+    ( object,
+      (.:),
+      withObject,
+      withText,
+      KeyValue((.=)),
+      ToJSON(toJSON),
+      FromJSON(parseJSON),
+      Value(String) )
+import Data.List qualified as List (foldl')
 import Data.Text (Text)
-import Data.Text qualified as Text (pack)
+import Data.Text qualified as Text (pack, splitOn)
 import Data.Text.Read qualified as Text.Read (decimal, double)
 import Data.Kind (Type)
 
@@ -44,11 +93,6 @@ import SwapiClient.Id qualified as Id
   , lukeSpeciesIds
   , lukeVehicleIds
   , lukeStarshipIds
-  -- , mkHomeworldId
-  -- , mkFilmId
-  -- , mkSpeciesId
-  -- , mkVehicleId
-  -- , mkStarshipId
   )
 
 --------------------------------------------------------------------------------
@@ -60,22 +104,53 @@ data BirthYear
   | UnknownBirthYear
   deriving Show
 
--- TODO(sekun): Add other colors
-data Color
-  = Blue
-  | Yellow
-  | Red
-  | Hazel
-  | NoColor
+data HairColor
+  = AuburnHair
+  | BlackHair
+  | BlondHair
+  | BrownHair
+  | GreyHair
+  | NoHairColor
+  | WhiteHair
   deriving Show
 
-newtype HairColor = HairColor Color
+newtype HairColors = HairColors [HairColor]
   deriving Show
 
-newtype SkinColor = SkinColor Color
+data SkinColor
+  = BlueSkin
+  | BrownSkin
+  | BrownMottleSkin
+  | DarkSkin
+  | FairSkin
+  | GoldSkin
+  | GreenSkin
+  | GreenTanSkin
+  | GreySkin
+  | LightSkin
+  | MetalSkin
+  | MottledGreenSkin
+  | OrangeSkin
+  | PaleSkin
+  | RedSkin
+  | SilverSkin
+  | TanSkin
+  | UnknownSkinColor
+  | WhiteSkin
+  | YellowSkin
   deriving Show
 
-newtype EyeColor = EyeColor Color
+data EyeColor
+  = BlackEye
+  | BlueEye
+  | BlueGreyEye
+  | GoldEye
+  | HazelEye
+  | OrangeEye
+  | PinkEye -- lmao
+  | RedEye
+  | UnknownEyeColor
+  | YellowEye
   deriving Show
 
 data Height
@@ -101,9 +176,9 @@ data Person = Person
   { pName             :: PersonName     -- Name of person
   , pHeight           :: Height         -- Height of person can be Nothing
   , pMass             :: Mass           -- Mass of person can be Nothing
-  , pHairColor        :: HairColor
-  , pSkinColor        :: SkinColor
-  , pEyeColor         :: EyeColor       -- Uh, eye color.
+  , pHairColor        :: HairColors
+  --, pSkinColor        :: [SkinColor]
+  --, pEyeColor         :: EyeColor       -- Uh, eye color.
   , pBirthYear        :: BirthYear      -- Relative to before/after Battle of Yavin
   , pGender           :: Gender         -- Gender according to SWAPI
   , pHomeworldId      :: HomeworldId    -- Homeworld IDs of character
@@ -116,6 +191,8 @@ data Person = Person
 
 --------------------------------------------------------------------------------
 -- INSTANCES
+-- TODO: Implement `FromJSON` and `ToJSON` instances for `SkinColor`
+-- TODO: Implement `FromJSON` and `ToJSON` instances for `EyeColor`
 
 instance FromJSON (Height :: Type) where
   parseJSON =
@@ -152,64 +229,68 @@ instance ToJSON (Mass :: Type) where
       UnknownMass -> String "unknown"
 
 -- TODO(sekun): Consider `DerivingVia`  for colors? A bit tiring to do by hand. :(
-instance FromJSON (HairColor :: Type) where
+instance FromJSON (HairColors :: Type) where
   parseJSON =
     withText "HairColor"
-      $ \case
-          "blue" -> pure (HairColor Blue)
-          "yellow" -> pure (HairColor Yellow)
-          "red" -> pure (HairColor Red)
-          "hazel" -> pure (HairColor Hazel)
-          "none" -> pure (HairColor NoColor)
-          _ -> fail "ERROR: Unexpected hair color value"
+      $ \hairText ->
+          let hairColors :: Either String [HairColor]
+              hairColors = mapM textToHairColor . Text.splitOn ", " $ hairText
+          in
+            case hairColors of
+              Right hc -> pure . HairColors $ hc
+              Left e -> fail e
 
-instance ToJSON (HairColor :: Type) where
-  toJSON (HairColor color) =
-    case color of
-      Blue -> String "blue"
-      Yellow -> String "yellow"
-      Red -> String "red"
-      Hazel -> String "hazel"
-      NoColor -> String "none"
+instance ToJSON (HairColors :: Type) where
+  toJSON (HairColors hairColors) =
+    String . colorsToText $ hairColors
+    where
+      colorsToText :: [HairColor] -> Text
+      colorsToText =
+        List.foldl'
+          (\hairColorsText color ->
+             if hairColorsText == ""
+             then hairColorToText color
+             else mconcat [hairColorsText, ", ", hairColorToText color])
+          mempty
 
-instance FromJSON (SkinColor :: Type) where
-  parseJSON = withText "SkinColor" $
-    \case
-      "blue" -> pure (SkinColor Blue)
-      "yellow" -> pure (SkinColor Yellow)
-      "red" -> pure (SkinColor Red)
-      "hazel" -> pure (SkinColor Hazel)
-      "none" -> pure (SkinColor NoColor)
-      _ -> fail "ERROR: Unexpected skin color value"
-
-instance ToJSON (SkinColor :: Type) where
-  toJSON (SkinColor color) =
-    case color of
-      Blue -> String "blue"
-      Yellow -> String "yellow"
-      Red -> String "red"
-      Hazel -> String "hazel"
-      NoColor -> String "none"
-
-instance FromJSON (EyeColor :: Type) where
-  parseJSON =
-    withText "EyeColor" $
-      \case
-        "blue" -> pure (EyeColor Blue)
-        "yellow" -> pure (EyeColor Yellow)
-        "red" -> pure (EyeColor Red)
-        "hazel" -> pure (EyeColor Hazel)
-        _ -> fail "ERROR: Unexpected eye color value"
-
-instance ToJSON (EyeColor :: Type) where
-  toJSON :: EyeColor -> Value
-  toJSON (EyeColor eyeColor) =
-    case eyeColor of
-      Blue -> String "blue"
-      Yellow -> String "yellow"
-      Red -> String "red"
-      Hazel -> String "hazel"
-      NoColor -> String "none"
+--instance FromJSON (SkinColor :: Type) where
+--  parseJSON = withText "SkinColor" $
+--    \case
+--      "blue" -> pure (SkinColor Blue)
+--      "yellow" -> pure (SkinColor Yellow)
+--      "red" -> pure (SkinColor Red)
+--      "hazel" -> pure (SkinColor Hazel)
+--      "none" -> pure (SkinColor NoColor)
+--      _ -> fail "ERROR: Unexpected skin color value"
+--
+--instance ToJSON (SkinColor :: Type) where
+--  toJSON (SkinColor color) =
+--    case color of
+--      Blue -> String "blue"
+--      Yellow -> String "yellow"
+--      Red -> String "red"
+--      Hazel -> String "hazel"
+--      NoColor -> String "none"
+--
+--instance FromJSON (EyeColor :: Type) where
+--  parseJSON =
+--    withText "EyeColor" $
+--      \case
+--        "blue" -> pure (EyeColor Blue)
+--        "yellow" -> pure (EyeColor Yellow)
+--        "red" -> pure (EyeColor Red)
+--        "hazel" -> pure (EyeColor Hazel)
+--        _ -> fail "ERROR: Unexpected eye color value"
+--
+--instance ToJSON (EyeColor :: Type) where
+--  toJSON :: EyeColor -> Value
+--  toJSON (EyeColor eyeColor) =
+--    case eyeColor of
+--      Blue -> String "blue"
+--      Yellow -> String "yellow"
+--      Red -> String "red"
+--      Hazel -> String "hazel"
+--      NoColor -> String "none"
 
 instance FromJSON (BirthYear :: Type) where
   -- TODO(sekun): Add instance type signature
@@ -263,8 +344,8 @@ instance FromJSON (Person :: Type) where
           <*> objPerson .: "height"
           <*> objPerson .: "mass"
           <*> objPerson .: "hair_color"
-          <*> objPerson .: "skin_color"
-          <*> objPerson .: "eye_color"
+        --  <*> objPerson .: "skin_color"
+        --  <*> objPerson .: "eye_color"
           <*> objPerson .: "birth_year"
           <*> objPerson .: "gender"
           <*> objPerson .: "homeworld"
@@ -280,8 +361,8 @@ instance ToJSON (Person :: Type) where
       , "height"     .= pHeight person
       , "mass"       .= pMass person
       , "hair_color" .= pHairColor person
-      , "skin_color" .= pSkinColor person
-      , "eye_color"  .= pEyeColor person
+ --     , "skin_color" .= pSkinColor person
+ --     , "eye_color"  .= pEyeColor person
       , "birth_year" .= pBirthYear person
       , "gender"     .= pGender person
       , "homeworld"  .= pHomeworldId person
@@ -300,9 +381,9 @@ lukeSkywalker =
     { pName        = PersonName "Luke Skywalker"
     , pHeight      = Height 172
     , pMass        = Mass 77
-    , pHairColor   = HairColor Yellow
-    , pSkinColor   = SkinColor Blue
-    , pEyeColor    = EyeColor Blue
+    , pHairColor   = HairColors [BlondHair, BrownHair]
+--    , pSkinColor   = SkinColor Blue
+--    , pEyeColor    = EyeColor Blue
     , pBirthYear   = BBY 19
     , pGender      = Male
     , pHomeworldId = Id.lukeHomeworldId
@@ -311,3 +392,28 @@ lukeSkywalker =
     , pVehicleIds  = Id.lukeVehicleIds
     , pStarshipIds = Id.lukeStarshipIds
     }
+
+--------------------------------------------------------------------------------
+-- Functions
+
+textToHairColor :: Text -> Either String HairColor
+textToHairColor hct = case hct of
+  "auburn"     -> Right AuburnHair
+  "black"      -> Right BlackHair
+  "blond"      -> Right BlondHair
+  "brown"      -> Right BrownHair
+  "grey"       -> Right GreyHair
+  "white"      -> Right WhiteHair
+  "n/a"        -> Right NoHairColor
+  "none"       -> Right NoHairColor
+  _            -> Left "ERROR: Invalid hair color value/format"
+
+hairColorToText :: HairColor -> Text
+hairColorToText hairColor = case hairColor of
+  AuburnHair -> "auburn"
+  BlackHair -> "black"
+  BlondHair -> "blond"
+  BrownHair -> "brown"
+  GreyHair -> "grey"
+  NoHairColor -> "none"
+  WhiteHair -> "white"
