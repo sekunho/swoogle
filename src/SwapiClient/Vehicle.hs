9821dd578198e6f4b31fba555de0c8692f69ed17
SEKUN <sekun@hey.com>
2022-01-30 09:41:31 +0800

    Add `FromJSON` instances for Vehicle types

--- src/SwapiClient/Vehicle.hs
+++ src/SwapiClient/Vehicle.hs
@@ -22,29 +22,43 @@ module SwapiClient.Vehicle
 
 --------------------------------------------------------------------------------
 
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson (parseJSON, (.:))
+import Data.Aeson.Types (FromJSON, ToJSON, Parser, Value)
+import Data.Aeson qualified as Aeson (withText, withObject)
+import Data.Kind (Type)
 import Data.Text (Text)
-import Data.Time
+import Data.Text qualified as Text (toLower)
+import Data.Text.Read qualified as Text.Read (double)
+import Data.Time (UTCTime)
 
 --------------------------------------------------------------------------------
 
 import SwapiClient.Id (PersonId, FilmId, VehicleId)
 import SwapiClient.Starship
+  ( CargoCapacity
+  , Consumable
+  , Cost
+  , Manufacturer
+  , MaxAtmospheringSpeed
+  , PassengerLimit
+  , RequiredCrew
+  , Wrapped (Wrapped)
+  )
 
 --------------------------------------------------------------------------------
 -- Data types
 
 newtype VehicleName = VehicleName Text
   deriving stock (Eq, Show)
-  deriving (FromJSON, ToJSON) via StarshipName
+  deriving ToJSON via (Wrapped Text)
 
 newtype VehicleModel = VehicleModel Text
   deriving stock (Eq, Show)
-  deriving (FromJSON, ToJSON) via StarshipName
+  deriving ToJSON via (Wrapped Text)
 
 newtype VehicleLength = VehicleLength Double
   deriving stock (Eq, Show)
-  deriving (FromJSON, ToJSON) via StarshipLength
+  deriving ToJSON via (Wrapped Double)
 
 data VehicleClass
   = VCWheeled
@@ -86,3 +100,60 @@ data Vehicle = Vehicle
 --------------------------------------------------------------------------------
 -- Instances
 
+instance FromJSON (VehicleName :: Type) where
+  parseJSON = Aeson.withText "VehicleName" (pure . VehicleName)
+
+instance FromJSON (VehicleModel :: Type) where
+  parseJSON = Aeson.withText "VehicleModel" (pure . VehicleModel)
+
+instance FromJSON (VehicleLength :: Type) where
+  parseJSON = Aeson.withText "VehicleLength" $
+    \val ->
+      case Text.Read.double val of
+        Right (vehicleLength, "") -> pure $ VehicleLength vehicleLength
+        Right _ -> fail "VehicleLength is just supposed to be a number"
+        Left s -> fail s
+
+instance FromJSON (VehicleClass :: Type) where
+  parseJSON =
+    Aeson.withText "VehicleClass" $
+      \val ->
+        case Text.toLower val of
+          "wheeled" -> pure VCWheeled
+          "repulsorcraft" -> pure VCRepulsorcraft
+          "starfighter" -> pure VCStarfighter
+          "airspeeder" -> pure VCAirspeeder
+          "space bomber" -> pure VCSpaceBomber
+          "assault walker" -> pure VCAssaultWalker
+          "walker" -> pure VCWalker
+          "sail barge" -> pure VCSailBarge
+          "repulsorcraft cargo skiff" -> pure VCRepulsorcraftCargoSkiff
+          "speeder" -> pure VCSpeeder
+          "landing craft" -> pure VCLandingCraft
+          "submarine" -> pure VCSubmarine
+          "gunship" -> pure VCGunship
+          "transport" -> pure VCTransport
+          _ -> fail "Unexpected value for VehicleClass"
+
+instance FromJSON (Vehicle :: Type) where
+  parseJSON :: Value -> Parser Vehicle
+  parseJSON =
+    Aeson.withObject "Vehicle" $
+      \val ->
+        Vehicle
+          <$> val .: "name"
+          <*> val .: "model"
+          <*> val .: "manufacturer"
+          <*> val .: "cost_in_credits"
+          <*> val .: "length"
+          <*> val .: "max_atmosphering_speed"
+          <*> val .: "crew"
+          <*> val .: "passengers"
+          <*> val .: "cargo_capacity"
+          <*> val .: "consumables"
+          <*> val .: "vehicle_class"
+          <*> val .: "pilots"
+          <*> val .: "films"
+          <*> val .: "created"
+          <*> val .: "edited"
+          <*> val .: "url"
