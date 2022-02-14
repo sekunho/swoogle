e17d0a519545ac459dffefccda66499f6142a405
SEKUN <sekun@hey.com>
2022-02-14 17:37:28 +0800

    Organize flake

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "SWAPI";
 
   inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    unstablepkgs.url    = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url      = "github:NixOS/nixpkgs";
+    flake-utils.url  = "github:numtide/flake-utils";
+    unstablepkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, flake-utils, unstablepkgs }:
@@ -17,19 +17,21 @@
         devShell = pkgs.mkShell rec {
           buildInputs = [
             pkgs.ghc                             # Haskell compiler
+
+            # Tooling
             pkgs.cabal-install                   # Build tool
             pkgs.haskell-language-server         # Haskell LSP
             pkgs.hlint                           # Linter
             pkgs.haskellPackages.implicit-hie    # To deal with HLS + cabal oddities
             pkgs.haskellPackages.stylish-haskell # Code formatter
             pkgs.haskell-ci                      # Github Actions generator
-            pkgs.zlib                            # I forgot why this was here lmao
 
             # Front-end
-            unstable.nodePackages.tailwindcss
+            unstable.nodePackages.tailwindcss    # Styling with utility classes
 
             # Misc.
-            pkgs.watchexec
+            pkgs.watchexec                       # Watch changes and execute something
+            pkgs.zlib                            # I forgot why this was here lmao
           ];
 
           LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
