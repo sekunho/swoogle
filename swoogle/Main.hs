96b82efc6708b85762d0e2d48d6c64e7290cbf88
SEKUN <sekun@hey.com>
2022-02-15 12:46:13 +0800

    Set `Content-Type` for `app.css`

--- swoogle/Main.hs
+++ swoogle/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Lucid
 import Web.Scotty (ScottyM)
-import Web.Scotty qualified as Scotty (scotty, html, get, param, file)
+import Web.Scotty qualified as Scotty (scotty, html, get, param, file, setHeader)
 
 import Swoogle.Views.Home qualified as Home (content)
 import Swoogle.Views.Layout qualified as Layout (root)
@@ -12,7 +12,10 @@ main = Scotty.scotty 3000 swapiWeb
 
 swapiWeb :: ScottyM ()
 swapiWeb = do
-  Scotty.get "/assets/:file" (Scotty.param "file" >>= Scotty.file . (<>) (staticPath <> "assets/"))
+  Scotty.get
+    "/assets/app.css"
+    (Scotty.setHeader "Content-Type" "text/css; charset=utf-8" >>
+      Scotty.file (staticPath <> "assets/app.css"))
 
   Scotty.get "/fonts/:file" (Scotty.param "file" >>= Scotty.file . (<>) (staticPath <> "fonts/"))
 
